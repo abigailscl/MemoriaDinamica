cad34904536b86bc5e8bed24e18bdb3464257c32
Abigail Cabascango <51092317+sacabascango1@users.noreply.github.com>
2020-06-02 02:25:53 -0500

    Implementacion mcm

--- Aplicativo.cpp
+++ Aplicativo.cpp
@@ -9,9 +9,11 @@
 	Vectores objVec;
 	int* vec = new int [10];
 	objVec.ingresarValores2(vec,10);
+	//objVec.ordenarVector(vec,10);
 	objVec.imprimirVector(vec, 10);
- 	cout<<"Ejecucion exitosa";
- 	
+	objVec.calcMCM(vec, 10);
+ 	cout<<"\nEjecucion exitosa";
+ 	delete(vec);
 	return 0;
  }
  
--- OperacionVenctor.h
+++ OperacionVenctor.h
@@ -23,14 +23,18 @@ public:
    void imprimirVector(int* vector, int dim);
    void encerarVector(int* vector, int dim);
    void ordenarVector(int* vector, int dim);
-   void imprimirMaxMin(int* vector, int dim);
+   void calcMCM(int* vector, int dim);
+   void calcMCD(int* vector, int dim);
    void ingresarValores2(int* vector, int dim);
 
 
 protected:
 private:
-   int minimo;
-   int maximo;
+	unsigned long int mcm(int *vector, int n);
+	bool esDivisible(int *vector, int dim, int numero);
+	int maxArr(int *vector, int dim);
+   int menor;
+   int mayor;
 
 
 };
@@ -42,19 +46,23 @@ void Vectores::imprimirVector(int* vector, int dim)
 {
 	for (int i = 0; i < dim; i++){
 		
-		cout << <*(vector+i)<< ", " ;
+		cout << *(vector+i) << "; ";
 	}
 }
 void Vectores:: encerarVector(int* vector, int dim)
 {
-	
+	for (int i = 0; i < dim; i++){
+		
+		*(vector+i) = 0;
+	}
 }
 void Vectores::ordenarVector(int* vector, int dim)
 {
 	
 }
-void Vectores::imprimirMaxMin(int* vector, int dim)
+void Vectores::calcMCM(int* vector, int dim)
 {
+	cout << "\nMCM = " << mcm(vector,dim);
 	
 }
 void Vectores::ingresarValores2(int *vector, int dim)
@@ -68,6 +76,38 @@ void Vectores::ingresarValores2(int *vector, int dim)
  	 
 }
 
+unsigned long int Vectores::mcm(int *vector, int dim){
+	int i,len;
+	unsigned long int intMcm = 1;
+	i = 0;
+	len = maxArr(vector,dim);
+	for(int i = 2; i <= len; i++){
+		while( esDivisible(vector,dim,i) ){
+			intMcm *= i;
+		}
+	}
+	return intMcm;
+}
 
-
+bool Vectores::esDivisible(int *vector, int dim, int numero){
+	bool divisible = false;
+	for(int i = 0; i < dim; i++){
+		if( *(vector+i)%numero == 0){
+			divisible = true;
+			*(vector+i) = *(vector+i)/numero;
+		}
+	}
+	return divisible;
+}
+int Vectores::maxArr(int *vector, int dim){
+	int max = -1;
+	if( dim > 1 ){
+		max = *(vector+0);
+		for(int i = 1; i < dim; i++){
+			if ( max < *(vector+i))
+				max = *(vector+i);
+		}
+	}
+	return max;
+}
 #endif
