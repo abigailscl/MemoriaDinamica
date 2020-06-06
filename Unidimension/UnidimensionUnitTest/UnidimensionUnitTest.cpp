d90a307eacde8cadec4a6bdc50509bd79ae736df
CharlRomero <romeokarlos12@gmail.com>
2020-06-06 14:00:22 -0500

    unit tests done

--- Unidimension/UnidimensionUnitTest/UnidimensionUnitTest.cpp
+++ Unidimension/UnidimensionUnitTest/UnidimensionUnitTest.cpp
@@ -19,7 +19,34 @@ namespace UnidimensionUnitTest
 			for (int i = 0; i < arr.getDimension(); i++)
 				*(arr.getArray() + i) = 2;
 			
-			Assert::AreEqual(2, op.mcm(arr));
+			Assert::AreEqual(8, op.mcm(arr));
+
+			Unidimensional arr2(3);
+
+			*(arr2.getArray()) = 2;			
+			*(arr2.getArray() + 1) = 3;
+			*(arr2.getArray() + 2) = 6;
+
+			Assert::AreEqual(6, op.mcm(arr2));
+		}
+
+		TEST_METHOD(mcdTest) {
+
+			Unidimensional arr(4);
+			Operation op;
+
+			for (int i = 0; i < arr.getDimension(); i++)
+				*(arr.getArray() + i) = 2;
+
+			Assert::AreEqual(2, op.mcd(arr));
+
+			Unidimensional arr2(3);
+
+			*(arr2.getArray()) = 2;
+			*(arr2.getArray() + 1) = 3;
+			*(arr2.getArray() + 2) = 6;
+
+			Assert::AreEqual(6, op.mcd(arr2));
 
 		}
 	};
