23d2a35eb6942a353ee361429ca7e759f598c953
CharlRomero <romeokarlos12@gmail.com>
2020-06-06 00:46:56 -0500

    unit tests done

--- MatrixApp/MatrixUnitTest/MatrixUnitTest.cpp
+++ MatrixApp/MatrixUnitTest/MatrixUnitTest.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "../MatrixApp/Matrix.h"
+#include "../MatrixApp/OperationMatrix.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -14,15 +15,54 @@ namespace MatrixUnitTest
 		{
 			Matrix matrix(2);
 			matrix.wax();
-			int** expected;
+			int** expected = new int*[matrix.getDimension()];
+			for (size_t i = 0; i < matrix.getDimension(); i++)
+				*(expected + i) = new int[matrix.getDimension()];
+			
 
-			for (int i = 0; i < 2; i++) {
-				for (int j = 0; j < 2; j++) {
+			for (size_t i = 0; i < matrix.getDimension(); i++) {
+				for (size_t j = 0; j < matrix.getDimension(); j++) {
 					*(*(expected + i) + j) = 0;
 				}
 			}
 
-			Assert::AreEqual(expected, matrix.getMatrix());
+			for (size_t i = 0; i < matrix.getDimension(); i++) {
+				for (size_t j = 0; j < matrix.getDimension(); j++) {
+					Assert::AreEqual(*(*(expected + i) + j), *(*(matrix.getMatrix() + i) + j));
+				}
+			}
+			
+		}
+
+		TEST_METHOD(operationTest) {
+
+			OperationMatrix op;
+
+			Matrix matrix(2);
+			matrix.wax();
+
+			for (size_t i = 0; i < matrix.getDimension(); i++) {
+				for (size_t j = 0; j < matrix.getDimension(); j++) {
+					*(*(matrix.getMatrix() + i) + j) = 2;
+				}
+			}
+
+			int** expected = new int* [matrix.getDimension()];
+			for (size_t i = 0; i < matrix.getDimension(); i++)
+				*(expected + i) = new int[matrix.getDimension()];
+
+
+			for (size_t i = 0; i < matrix.getDimension(); i++) {
+				for (size_t j = 0; j < matrix.getDimension(); j++) {
+					*(*(expected + i) + j) = 32;
+				}
+			}
+
+			for (size_t i = 0; i < matrix.getDimension(); i++) {
+				for (size_t j = 0; j < matrix.getDimension(); j++) {
+					Assert::AreEqual(*(*(expected + i) + j), *(*(op.multiplySameMatrix(matrix, 3).getMatrix() + i) + j));
+				}
+			}
 		}
 	};
 }
