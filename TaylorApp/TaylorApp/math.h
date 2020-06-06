32f0cc8c2c39d1c02a6b5fdf28915c3d362100d5
CharlRomero <romeokarlos12@gmail.com>
2020-06-06 17:48:22 -0500

    class ready

--- TaylorApp/TaylorApp/math.h
+++ TaylorApp/TaylorApp/math.h
@@ -1,8 +1,28 @@
 #pragma once
+
+#if !defined(__math_h)
+#define __math_h
+
 class math
 {
 public:
-	static int pow(int, int);
-	static long int factorial(int);
+	int pow(int, int);
+	long int factorial(int);
 };
 
+long int math::factorial(int value) {
+	if (value == 0)
+		return 1;
+	else
+		return value * factorial(value - 1);
+}
+
+int math::pow(int base, int exponent) {
+
+	if (exponent == 1)
+		return base;
+	else
+		return base * pow(base, exponent - 1);
+
+}
+#endif
