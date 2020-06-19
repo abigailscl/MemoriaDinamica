d907437eb58c6a45ff334b5454566bf925004a90
CharlRomero <romeokarlos12@gmail.com>
2020-06-18 23:55:54 -0500

    ready

--- MatrixApp/MatrixApp/Matrix.h
+++ MatrixApp/MatrixApp/Matrix.h
@@ -41,10 +41,50 @@ private:
     int dimension;
 };
 
+template <> class Matrix<int> {
+public:
+	void fill();
+	void wax();
+	void print();
+	int** getMatrix(void);
+	void setMatrix(int**);
+	int  getDimension();
+	void setDimension(int);
+	Matrix(int);
+	~Matrix();
+private:
+	int** matrix;
+	int dimension;
+};
+
+template <> class Matrix<double> {
+public:
+	void fill();
+	void wax();
+	void print();
+	double** getMatrix(void);
+	void setMatrix(double**);
+	int  getDimension();
+	void setDimension(int);
+	Matrix(int);
+	~Matrix();
+private:
+	double** matrix;
+	int dimension;
+};
+
 template <class T> T**  Matrix<T>::getMatrix(void) { return matrix; }
 template <class T> void Matrix<T>::setMatrix(T** matrix) { this->matrix = matrix; }
 template <class T> int  Matrix<T>::getDimension(void) { return dimension; }
 template <class T> void Matrix<T>::setDimension(int dimension) { this->dimension = dimension; }
+int**		Matrix<int>::getMatrix(void) { return this->matrix = matrix; }
+void		Matrix<int>::setMatrix(int** matrix) { this->matrix = matrix; }
+int			Matrix<int>::getDimension(void) { return dimension; }
+void        Matrix<int>::setDimension(int dimension) { this->dimension = dimension; }
+double**	Matrix<double>::getMatrix(void) { return this->matrix = matrix; }
+void        Matrix<double>::setMatrix(double** matrix) { this->matrix = matrix; }
+int			Matrix<double>::getDimension(void) { return dimension; }
+void        Matrix<double>::setDimension(int dimension) { this->dimension = dimension; }
 
 template <class T> Matrix<T>::Matrix(int dimension) {
 
@@ -57,20 +97,41 @@ template <class T> Matrix<T>::Matrix(int dimension) {
 
 }
 template <class T> Matrix<T>::~Matrix() {}
+Matrix<int>::Matrix(int dimension) {
+
+	this->dimension = dimension;
+	matrix = (int**)malloc(dimension * sizeof(int*));
+
+	for (size_t i = 0; i < dimension; i++) {
+		*(matrix + i) = (int*)malloc(dimension * sizeof(int));
+	}
+
+}
+Matrix<int>::~Matrix() {}
+Matrix<double>::Matrix(int dimension) {
+
+	this->dimension = dimension;
+	matrix = (double**)malloc(dimension * sizeof(double*));
+
+	for (size_t i = 0; i < dimension; i++) {
+		*(matrix + i) = (double*)malloc(dimension * sizeof(double));
+	}
+
+}
+Matrix<double>::~Matrix() {}
 
 template <class T> void Matrix<T>::fill() {
 
 	string str;
-	InputData<T> enter;
+	InputData<double> enter;
 	int value;
 	wax();
 	for (size_t i = 0; i < getDimension(); i++) {
 		for (size_t j = 0; j < getDimension(); j++) {
-			value = atoi(enter.matrizInteger(i, j).c_str());
+			value = atoi(enter.matrixFloatDouble(i, j).c_str());
 			*(*(getMatrix() + i) + j) = value;
 		}
 	}
-
 }
 template <class T> void Matrix<T>::print() {
 
@@ -85,6 +146,68 @@ template <class T> void Matrix<T>::print() {
 
 }
 template <class T> void Matrix<T>::wax() {
+
+	for (size_t i = 0; i < getDimension(); i++)
+		for (size_t j = 0; j < getDimension(); j++)
+			*(*(getMatrix() + i) + j) = 0;
+
+}
+void Matrix<int>::fill() {
+
+	string str;
+	InputData<int> enter;
+	int value;
+	wax();
+	for (size_t i = 0; i < getDimension(); i++) {
+		for (size_t j = 0; j < getDimension(); j++) {
+			value = atoi(enter.matrixInteger(i, j).c_str());
+			*(*(getMatrix() + i) + j) = value;
+		}
+	}
+}
+void Matrix<int>::print() {
+
+	cout << "\nMatriz resultante:\n";
+
+	for (size_t i = 0; i < getDimension(); i++) {
+		for (size_t j = 0; j < getDimension(); j++) {
+			cout << *(*(getMatrix() + i) + j) << " ";
+		}
+			cout << "\n";
+	}
+}
+void Matrix<int>::wax() {
+
+	for (size_t i = 0; i < getDimension(); i++)
+		for (size_t j = 0; j < getDimension(); j++)
+			*(*(getMatrix() + i) + j) = 0;
+
+}
+void Matrix<double>::fill() {
+
+	string str;
+	InputData<double> enter;
+	int value;
+	wax();
+	for (size_t i = 0; i < getDimension(); i++) {
+		for (size_t j = 0; j < getDimension(); j++) {
+			value = atoi(enter.matrixFloatDouble(i, j).c_str());
+			*(*(getMatrix() + i) + j) = value;
+		}
+	}
+}
+void Matrix<double>::print() {
+
+	cout << "\nMatriz resultante:\n";
+
+	for (size_t i = 0; i < getDimension(); i++) {
+		for (size_t j = 0; j < getDimension(); j++) {
+			cout << *(*(getMatrix() + i) + j) << " ";
+		}
+		cout << "\n";
+	}
+}
+void Matrix<double>::wax() {
 
 	for (size_t i = 0; i < getDimension(); i++)
 		for (size_t j = 0; j < getDimension(); j++)
